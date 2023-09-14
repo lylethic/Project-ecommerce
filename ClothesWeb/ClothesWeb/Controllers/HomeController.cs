42fd6ab425ceab2b6ca0e81c4e2662c0c72f4928
camly <lely0168340@gmail.com>
2023-09-14 09:45:22 +0700

    14/9/2023

---
+++ ClothesWeb/ClothesWeb/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using ClothesWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ClothesWeb.Controllers
+{
+  public class HomeController : Controller
+  {
+    //private readonly ILogger<HomeController> _logger;
+
+    //public HomeController(ILogger<HomeController> logger)
+    //{
+    //  _logger = logger;
+    //}
+
+    public IActionResult Index()
+    {
+      return View();
+    }
+
+    public IActionResult About()
+    {
+      return View();
+    }
+
+    public IActionResult Shop()
+    {
+      return View();
+    }
+
+    public IActionResult Blog()
+    {
+      return View();
+    }
+
+    public IActionResult Contact()
+    {
+      return View();
+    }
+
+    public IActionResult Login()
+    {
+      return View();
+    }
+
+    public IActionResult SignUp()
+    {
+      return View();
+    }
+
+    public IActionResult Cart()
+    {
+      return View();
+    }
+  }
+}
